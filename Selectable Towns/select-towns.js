775b065324c0fe74187714236faa7bbbd7cab269
Stefani Nedelcheva <49128892+stefani-nedelcheva@users.noreply.github.com>
2019-09-02 15:47:36 +0300

    select-towns.js

---
+++ Selectable Towns/select-towns.js
@@ -0,0 +1,24 @@
+function attachEvents() {
+	$('#items li').click(selectTown);
+	let towns = [];
+
+	function selectTown() {
+		let currentListItem = $(this);
+
+		if (currentListItem.attr('data-selected') === 'true') {
+			currentListItem.attr('data-selected', 'false');
+			towns.splice(towns.indexOf(currentListItem.text()), 1);
+			currentListItem.css('background', '');
+		} else {
+			currentListItem.attr('data-selected', 'true');
+			towns.push(currentListItem.text());		
+			currentListItem.css('background', '#DDD');
+		}		
+	}
+
+	$('#showTownsButton').click(showTowns);
+
+	function showTowns() {
+		$('#selectedTowns').text(towns.join(', '));
+	}
+}
