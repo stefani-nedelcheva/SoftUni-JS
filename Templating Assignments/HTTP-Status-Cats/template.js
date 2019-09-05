5feaf831dc34453164338b6dba18c9ece641690a
Stefani Nedelcheva <49128892+stefani-nedelcheva@users.noreply.github.com>
2019-09-05 17:04:18 +0300

    Create template.js

---
+++ Templating Assignments/HTTP-Status-Cats/template.js
@@ -0,0 +1,18 @@
+$(() => {
+    renderCatTemplate();
+
+    async function renderCatTemplate() {
+        let catsTemplate = await $.ajax({
+            url: './catsTemplate.html'
+        });
+        let compiledTemplate = Handlebars.compile(catsTemplate);
+        let context = {
+            cats: window.cats
+        };
+        $('#allCats').html(compiledTemplate(context));
+    }
+});
+
+function showInfo(id) {
+    $(`#${id}`).toggle();
+}
